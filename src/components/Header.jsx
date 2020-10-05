6cc638696c9ee7c3ff6eeb0f99b0a6db4cc549f2
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-10-05 20:25:35 +0300

    changed Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://jlptsensei.com/wp-content/uploads/2015/02/JLPT-logo-600x298.jpg"
+        alt="logo1"
+      />
+    </header>
+  );
+};
+
+export default Header;
