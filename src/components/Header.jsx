a4e47c1276d00cd75362f3598e1b87b6aa190a5a
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-10-05 20:47:17 +0300

    test2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="header">
       <img
         src="https://jlptsensei.com/wp-content/uploads/2015/02/JLPT-logo-600x298.jpg"
-        alt="logo"
+        alt="logo1"
       />
     </header>
   );
