c7422c588b7aa950b57ac41598c7cc782040e00e
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-10-08 09:55:03 +0300

    08 10 morning

---
+++ src/components/Navbar/NavElement/NavElement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./NavElement.module.css";
+
+const NavElement = (props) => {
+  return (
+    <div className={s.item}>
+      <a href="https://www.google.com" target="blank">
+        {props.title}
+      </a>
+    </div>
+  );
+};
+
+export default NavElement;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,26 @@
 import React from "react";
 import s from "./Navbar.module.css";
+import NavElement from "./NavElement/NavElement";
+
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.itemWrapper}>
         <div className={s.item}>
-          <a href="https://www.google.com" target="blank">
-            Home Page
-          </a>
+          <NavElement title = "Home page"/>
         </div>
         <div className={s.item}>
-          <a href="https://www.google.com" target="blank">
-            Settings
-          </a>
+          <NavElement title = "News"/>
         </div>
         <div className={s.item}>
-          <a href="https://www.google.com" target="blank">
-            N4
-          </a>
+          <NavElement title = "Messages"/>
         </div>
         <div className={s.item}>
-          <a href="https://www.google.com" target="blank">
-            N3
-          </a>
+          <NavElement title = "Settings"/>
         </div>
         <div className={s.item}>
-          <a href="https://www.google.com" target="blank">
-            N2
-          </a>
+          <NavElement title = "Information"/>
         </div>
       </div>
     </nav>
