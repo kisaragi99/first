664e4c200d6bce10c6e378c937b92ff0d69432a6
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-10-29 22:55:37 +0300

    minor change

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -2,19 +2,21 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 const dialogsReducer = (state, action) => {
-    if (action.type === ADD_MESSAGE) {
-        let text = state.newMessageTempText;
-        let newMessage = {id: 9, message: text};
-        state.messages.push(newMessage);
-        state.newMessageTempText = '';
+    switch (action.type) {
+        case ADD_MESSAGE:
+            let text = state.newMessageTempText;
+            let newMessage = {id: 9, message: text};
+            state.messages.push(newMessage);
+            state.newMessageTempText = '';
+            return state;
 
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageTempText = action.newMessageText;
+            return state;
 
-    } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageTempText = action.newMessageText;
-
+        default:
+                return state;
     }
-        return state;
-
 }
 
 
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -2,16 +2,19 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
-    if (action.type === ADD_POST) {
-        let newPost = {id: 7, message: state.newPostText, likesCount: 9}
-        state.postsData.push(newPost);
-        state.newPostText = '';
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {id: 7, message: state.newPostText, likesCount: 9}
+            state.postsData.push(newPost);
+            state.newPostText = '';
+            return state;
 
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newText;
-
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+        default: return state;
     }
-        return state;
+
 }
 
 export default profileReducer;
