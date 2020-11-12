2763b30b9a8b7bfe01d37158a935b9337e34a91b
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-11-12 22:33:21 +0300

    first class component

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,38 +1,53 @@
 import React from "react";
 import s from "./Friends.module.css"
-import FriendsItem from "./FriendsItem/FriendsItem";
 import * as axios from "axios";
 import avatar from "../../assets/images/avatar.png"
 
-const Friends = (props) => {
-
-    let getUsers = () => {
-        if (props.friends.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setFriends(response.data.items)
-            })
-        }
+class Friends extends React.Component {
+constructor(props) {
+    super(props);
+    if (this.props.friends.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setFriends(response.data.items)
+        })
     }
-
-    let friendss = props.friends.map((friend => <FriendsItem name={friend.name}
-                                                             avatar={friend.photos.small != null ? friend.photos.small : avatar}
-                                                             id={friend.id}
-                                                             status={friend.status}
-                                                             location={friend.location}
-                                                             followed={friend.followed}
-                                                             follow={props.follow}
-                                                             unfollow={props.unfollow}/>));
-
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
-            <div className={s.wrapper}>
-                {friendss}
-            </div>
-        </div>
-
-    )
 }
+
+    render() {
+        return ( <div>
+            {
+            this.props.friends.map((friend =>
+                <div className={s.friendsBox} key={friend.id}>
+                    <div>
+                        <div>
+                            <img src={friend.photos.small != null ? friend.photos.small : avatar}
+                                 className={s.friendsAvatar}/>
+                        </div>
+                        <div> {friend.followed ? <button onClick={() => {
+                                this.props.unfollow(friend.id)
+                            }}> Unfollow </button> :
+                            <button onClick={() => {
+                                this.props.follow(friend.id)
+                            }}>Follow</button>}
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <div>{friend.name}</div>
+                            <div>{friend.status}</div>
+                        </div>
+                        <div>
+                            <div>{"props.location.country"}</div>
+                            <div>{"props.location.city"}</div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>)
+    }
+}
+
 export default Friends;
 
 
