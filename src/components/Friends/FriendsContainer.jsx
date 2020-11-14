4078eebb6a4a0bfc7cde0727ae314e11c5b71605
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-11-14 21:30:05 +0300

    new presentational and conatainer

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-import Friends from "./Friends";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setTotalFriendsCountAC, setUsersAC, unfollowAC} from "../../redux/friendsReducer";
+import * as axios from "axios";
+import FriendsPresentational from "./FriendsPresentational";
+
+class FriendsContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setFriends(response.data.items)
+            this.props.setTotalFriendsCount(response.data.totalCount)
+        })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setFriends(response.data.items);
+
+        })
+    }
+
+    render() {
+        return <FriendsPresentational friends={this.props.friends}
+                                      follow={this.props.follow}
+                                      unfollow={this.props.unfollow}
+                                      currentPage={this.props.currentPage}
+                                      onPageChanged={this.onPageChanged}
+                                      totalFriendsCount={this.props.totalFriendsCount}
+                                      pageSize={this.props.pageSize}/>
+    }
+}
+
 
 let mapStateToProps = (state) => {
     return {
@@ -30,8 +60,8 @@ let mapDispatchToProps = (dispatch) => {
         }
     }
 }
-const FriendsContainer = connect(mapStateToProps, mapDispatchToProps)(Friends)
+export default connect(mapStateToProps, mapDispatchToProps)(FriendsContainer)
 
-export default FriendsContainer;
+
 
 
---
+++ src/components/Friends/FriendsPresentational.js
@@ -0,0 +1,53 @@
+import React from "react";
+import s from "./Friends.module.css";
+import avatar from "../../assets/images/avatar.png";
+
+let FriendsPresentational = (props) => {
+
+    let pagesCount = Math.ceil(props.totalFriendsCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+    return <div>
+
+        {
+            props.friends.map((friend =>
+                    <div className={s.friendsBox} key={friend.id}>
+                        <div>
+                            <div>
+                                <img src={friend.photos.small != null ? friend.photos.small : avatar}
+                                     className={s.friendsAvatar}/>
+                            </div>
+                            <div> {friend.followed ? <button onClick={() => {
+                                    props.unfollow(friend.id)
+                                }}> Unfollow </button> :
+                                <button onClick={() => {
+                                    props.follow(friend.id)
+                                }}>Follow</button>}
+                            </div>
+                        </div>
+
+                        <div>
+                            <div>
+                                <div>{friend.name}</div>
+                                <div>{friend.status}</div>
+                            </div>
+                            <div>
+                                <div>{"Some Country"}</div>
+                                <div>{"Some City"}</div>
+                            </div>
+                        </div>
+                    </div>
+            ))}
+        <div> {pages.map(p => {
+            return <span className={props.currentPage === p && s.selectedPage}
+                         onClick={(e) => {
+                             props.onPageChanged(p)
+                         }}>{`${p} `}</span>
+        })}
+        </div>
+    </div>
+}
+export default FriendsPresentational;
