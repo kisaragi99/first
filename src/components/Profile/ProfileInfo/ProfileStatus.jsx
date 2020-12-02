8b388f74be779c29c0e9e5679274cb2262b20703
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-12-02 19:47:41 +0300

    minor

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -8,13 +8,13 @@ class ProfileStatus extends React.Component {
         editMode: false
     }
 
-    activateEditMode(){
+    activateEditMode = () => {
         this.setState({ // setState - is an async function.
             editMode: true
         })
     }
 
-    deactivateEditMode(){
+    deactivateEditMode = () => {
         this.setState({ // setState - is an async function.
             editMode: false
         })
@@ -25,11 +25,11 @@ class ProfileStatus extends React.Component {
 
                     {!this.state.editMode &&
                         <div>
-                            <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                            <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                         </div>}
                     {this.state.editMode &&
                         <div>
-                            <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}></input>
+                            <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}></input>
                         </div>}
 
                 </div>)
