152783f77a0bab8b340ec094e23e7ac8e6149bdf
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-12-17 21:22:16 +0300

    refactored FormControl

--- src/components/common/FormsControls/FormControls.js
+++ src/components/common/FormsControls/FormControls.js
@@ -2,40 +2,32 @@ import React from "react";
 import s from "./FormControls.module.css";
 
 
-export const Textarea = (props) => {
-
+export const FormControl = (props) => {
     const hasError = props.meta.touched && props.meta.error;
 
     return (
         <div className={s.formControl + " " + (hasError ? s.error : null)}>
             <div>
-                <textarea {...props.input}{...props}/>
+                {props.children}
             </div>
             {hasError && <span>{props.meta.error}</span>}
         </div>
     )
 }
 
+
+export const Textarea = (props) => {
+    return <FormControl {...props}><textarea {...props.input}{...props}/></FormControl>
+}
 
 // - Здесь не всегда успевает прогрузиться что-то из пропсов, поэтому иногда просто не работает подсветка. (Профиль)
 // + к этому, можно запостить пустую строку, а это значит что валдиатор не работает.
 // P.S. - через 15 минут почему-то все нормализовалось.
-
+// P.S. - 2   - снова в профайле проблемы. А в сообщениях нету проблемы...
 
 export const Input = (props) => {
-
-    const hasError = props.meta.touched && props.meta.error;
-
-    return (
-        <div className={s.formControl + " " + (hasError ? s.error : null)}>
-            <div>
-                <input {...props.input}{...props}/>
-            </div>
-            {hasError && <span>{props.meta.error}</span>}
-        </div>
-    )
+    return <FormControl {...props}><input{...props.input}{...props}/></FormControl>
 }
-
 
 // import React from "react";
 //
