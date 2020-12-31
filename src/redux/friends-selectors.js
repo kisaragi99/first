d41b501fa604c6c2c6716ed309b28d29dddefc00
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2020-12-31 22:12:39 +0300

    minor

--- src/redux/friends-selectors.js
+++ src/redux/friends-selectors.js
@@ -1,20 +1,19 @@
-
-export const getFriends = (state) =>{
+export const getFriends = (state) => {
     return state.friendsPage.friends;
 }
-export const getTotalFriendsCount = (state) =>{
+export const getTotalFriendsCount = (state) => {
     return state.friendsPage.totalFriendsCount;
 }
-export const getPageSize = (state) =>{
+export const getPageSize = (state) => {
     return state.friendsPage.pageSize;
 }
-export const getCurrentPage = (state) =>{
+export const getCurrentPage = (state) => {
     return state.friendsPage.currentPage;
 }
-export const getIsLoading = (state) =>{
+export const getIsLoading = (state) => {
     return state.friendsPage.isLoading;
 }
-export const getFollowingInProcess = (state) =>{
+export const getFollowingInProcess = (state) => {
     return state.friendsPage.followingInProcess;
 }
 
