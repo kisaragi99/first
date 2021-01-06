c197c98ed81031c804be0f155f1d9543449a864b
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-01-06 15:49:19 +0300

    forms needed to be written on formik instead of reduxForm

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,21 +5,22 @@ import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../utils/validators";
 import {Textarea} from "../../common/FormsControls/FormControls";
 
-const maxLength100 =  maxLengthCreator(100);
+const maxLength100 = maxLengthCreator(100);
 
 const MyPosts = (props) => {
 
     let postsElements = props.profilePage.postsData.map((post => <Post message={post.message}
                                                                        likesCount={post.likesCount}
-                                                                       key ={post.id}/>))
+                                                                       key={post.id}/>))
 
-    const MyPostsForm = (props) =>{
-        return(
+    const MyPostsForm = (props) => {
+        return (
             <form onSubmit={props.handleSubmit}>
                 <h3 className={s.greet}>Hi, my name is {props.name}, and these are my posts.</h3>
                 <div className={s.makePost}>
                     <div className={s.text1}>
-                        <Field placeholder={"Enter Your Post Here"} name={"newPostBody"} component={Textarea} validate={[required,maxLength100]}/>
+                        <Field placeholder={"Enter Your Post Here"} name={"newPostBody"} component={Textarea}
+                               validate={[required, maxLength100]}/>
                     </div>
                     <div>
                         <button className={s.button1}>
@@ -40,7 +41,6 @@ const MyPosts = (props) => {
     let addPost = (value) => {
         props.addPost(value.newPostBody);
     }
-
 
     return (
         <MyPostsReduxForm onSubmit={addPost}/>
--- src/components/common/FormsControls/FormControls.js
+++ src/components/common/FormsControls/FormControls.js
@@ -1,7 +1,6 @@
 import React from "react";
 import s from "./FormControls.module.css";
 
-
 export const FormControl = (props) => {
     const hasError = props.meta.touched && props.meta.error;
 
@@ -15,21 +14,18 @@ export const FormControl = (props) => {
     )
 }
 
-
 export const Textarea = (props) => {
     return <FormControl {...props}><textarea {...props.input}{...props}/></FormControl>
 }
 
-// - Здесь не всегда успевает прогрузиться что-то из пропсов, поэтому иногда просто не работает подсветка. (Профиль)
+// - Здесь не всегда успевает прогрузиться что-то из пропсов, поэтому иногда просто не работает подсветка. (<Profile/>)
 // + к этому, можно запостить пустую строку, а это значит что валдиатор не работает.
 // P.S. - через 15 минут почему-то все нормализовалось.
-// P.S. - 2   - снова в профайле проблемы. А в сообщениях нету проблемы...
+// P.S. - 2   - снова в профайле проблемы. А в <Messages/> нету проблемы...
 
 export const Input = (props) => {
     return <FormControl {...props}><input{...props.input}{...props}/></FormControl>
 }
-
-
 // import React from "react";
 //
 // export const Textarea = ({input, meta, ...props}) =>{
