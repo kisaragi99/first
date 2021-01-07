a81a7f1c9b705539a09ccfdabfd1fae4e214ef71
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-01-07 23:17:11 +0300

    mypost shouldComponentUpdate in process

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,44 +7,46 @@ import {Textarea} from "../../common/FormsControls/FormControls";
 
 const maxLength100 = maxLengthCreator(100);
 
-const MyPosts = (props) => {
+class MyPosts extends React.Component {
+    render() {
+        console.log("myposts rendered")
+        let postsElements = this.props.profilePage.postsData.map((post => <Post message={post.message}
+                                                                                likesCount={post.likesCount}
+                                                                                key={post.id}/>))
 
-    let postsElements = props.profilePage.postsData.map((post => <Post message={post.message}
-                                                                       likesCount={post.likesCount}
-                                                                       key={post.id}/>))
-
-    const MyPostsForm = (props) => {
-        return (
-            <form onSubmit={props.handleSubmit}>
-                <h3 className={s.greet}>Hi, my name is {props.name}, and these are my posts.</h3>
-                <div className={s.makePost}>
-                    <div className={s.text1}>
-                        <Field placeholder={"Enter Your Post Here"} name={"newPostBody"} component={Textarea}
-                               validate={[required, maxLength100]}/>
+        const MyPostsForm = (props) => {
+            return (
+                <form onSubmit={props.handleSubmit}>
+                    <h3 className={s.greet}>Hi, my name is {props.name}, and these are my posts.</h3>
+                    <div className={s.makePost}>
+                        <div className={s.text1}>
+                            <Field placeholder={"Enter Your Post Here"} name={"newPostBody"} component={Textarea}
+                                   validate={[required, maxLength100]}/>
+                        </div>
+                        <div>
+                            <button className={s.button1}>
+                                Post
+                            </button>
+                        </div>
                     </div>
                     <div>
-                        <button className={s.button1}>
-                            Post
-                        </button>
+                        {postsElements}
                     </div>
-                </div>
-                <div>
-                    {postsElements}
-                </div>
-            </form>
-        )
+                </form>
+            )
+        }
+
+        const MyPostsReduxForm = reduxForm({form: "myPostsForm"})(MyPostsForm)
+
+
+        let addPost = (value) => {
+            this.props.addPost(value.newPostBody);
+        }
+
+        return (
+            <MyPostsReduxForm onSubmit={addPost}/>
+        );
     }
-
-    const MyPostsReduxForm = reduxForm({form: "myPostsForm"})(MyPostsForm)
-
-
-    let addPost = (value) => {
-        props.addPost(value.newPostBody);
-    }
-
-    return (
-        <MyPostsReduxForm onSubmit={addPost}/>
-    );
-};
+}
 
 export default MyPosts;
