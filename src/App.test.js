617db6ad8f7cca38d5c271b499c58e294a4d91fe
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-01-25 23:48:45 +0300

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import MainApp from "./App";
-
-test('renders learn react link', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainApp/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/utils/object-helpers.js
+++
@@ -1,9 +0,0 @@
-export let updateObjectInArray = (items, itemId, objPropName, newObjProps) => {
-    return items.map(f => {
-        if (f[objPropName] === itemId) {
-            return {...f, ...newObjProps}
-        }
-        return f
-    })
-}
-
