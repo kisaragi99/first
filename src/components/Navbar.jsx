864e39f184e916bb943fe020fa639e5475ff0b13
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-01-25 23:17:30 +0300

    merge

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a href="https://www.google.com" target="blank">
+          Home Page
+        </a>
+      </div>
+      <div>
+        <a href="https://www.google.com" target="blank">
+          Settings
+        </a>
+      </div>
+      <div>
+        <a href="https://www.google.com" target="blank">
+          N4
+        </a>
+      </div>
+      <div>
+        <a href="https://www.google.com" target="blank">
+          N3
+        </a>
+      </div>
+      <div>
+        <a href="https://www.google.com" target="blank">
+          N2
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+        <div className="content-image">
+          <img
+            src="https://image.freepik.com/free-photo/wide-asphalt-road-with-buildings-on-the-horizon_1127-2192.jpg"
+            alt="mountains"
+          ></img>
+        </div>
+        <div className="avatar-description">
+          <img
+            src="https://m.media-amazon.com/images/I/41ms2c-lG4L._AC_SS350_.jpg"
+            alt="japanese-flag"
+          ></img>
+        </div>
+        <div className = "posts">
+          <div className = "new-post">
+
+          </div>
+        </div>
+
+      </div>
+  );
+};
+
+export default Profile;
