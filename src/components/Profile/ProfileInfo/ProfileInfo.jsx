340bd07e8e7e48fc2a7043b0dd28b5ccf85ad857
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-01-27 21:51:21 +0300

    started to refractor the ProfileInfo component minor

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -17,12 +17,16 @@ const ProfileInfo = ({isOwner ,profile, status, updateStatus, savePhoto}) => {
                 <img src={profile.photos.large || avatar} className={s.avatar} alt='avatar'/>
                 <div className={s.description}>
                     <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
+
+                    {/*Вынести дивку в отдельный компонент. Компонент рендерить через Object.keys*/}
                     <div className={s.profileInfo}><div className={s.profileInfoTitle}>Name:</div> {profile.fullName}</div>
                     <div className={s.profileInfo}><div className={s.profileInfoTitle}>About me:</div> {profile.aboutMe} </div>
                     <div className={s.profileInfo}><div className={s.profileInfoTitle}>FB:</div> <a href={profile.contacts.facebook}>{profile.contacts.facebook}</a> </div>
                     <div className={s.profileInfo}><div className={s.profileInfoTitle}>VK:</div> <a href={profile.contacts.vk}>{profile.contacts.vk}</a> </div>
                     <div className={s.profileInfo}><div className={s.profileInfoTitle}>Twitter: </div> <a href={profile.contacts.twitter}>{profile.contacts.twitter}</a> </div>
                     <div className={s.profileInfo}><div className={s.profileInfoTitle}>GitHub:</div> <a href={profile.contacts.github}>{profile.contacts.github}</a> </div>
+
+
                 </div>
                 {isOwner && <input type="file" onChange={onMainPhotoSelected}/>}
             </div>
