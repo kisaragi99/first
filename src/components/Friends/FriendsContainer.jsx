c67a58747e2772390ec67fc58b80f8f962e63d8e
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-02-27 17:54:31 +0300

    fixed pagination render issue in friendsContainer with isLoading prop

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -21,28 +21,27 @@ class FriendsContainer extends React.Component {
 
     componentDidMount() {
         const {getUsers, currentPage, pageSize} = this.props;
-        getUsers(currentPage, pageSize)
-    }
+        getUsers(currentPage, pageSize);
+    };
 
     onPageChanged = (pageNumber) => {
         const {getUsers, pageSize} = this.props;
-        getUsers(pageNumber, pageSize)
-    }
+        getUsers(pageNumber, pageSize);
+    };
 
     render() {
         return (<>
-            <FriendsPresentational friends={this.props.friends}
+            {this.props.isLoading ? <Loader/> : <FriendsPresentational friends={this.props.friends}
                                    follow={this.props.follow}
                                    unfollow={this.props.unfollow}
                                    currentPage={this.props.currentPage}
                                    onPageChanged={this.onPageChanged}
                                    totalFriendsCount={this.props.totalFriendsCount}
                                    pageSize={this.props.pageSize}
-                                   followingInProcess={this.props.followingInProcess}/>
-            {this.props.isLoading ? <Loader/> : null}
+                                   followingInProcess={this.props.followingInProcess}/>}
         </>)
-    }
-}
+    };
+};
 
 let mapStateToProps = (state) => {
     return {
@@ -52,8 +51,8 @@ let mapStateToProps = (state) => {
         currentPage: getCurrentPage(state),
         isLoading: getIsLoading(state),
         followingInProcess: getFollowingInProcess(state)
-    }
-}
+    };
+};
 
 export default compose(
     connect(mapStateToProps, {follow, unfollow, toggleFollowingProcess, getUsers})
