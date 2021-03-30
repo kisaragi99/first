73d5cb7424a91a514eddae1cca9407e23b952e24
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-03-30 21:10:21 +0300

    sender and recipient photo aqcuired and added to UI

--- src/components/Dialogs/DialogPrivatePage/DialogPrivatePage.jsx
+++ src/components/Dialogs/DialogPrivatePage/DialogPrivatePage.jsx
@@ -2,33 +2,40 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import s from "./DialogPrivatePage.module.css"
 import { withRouter } from "react-router-dom";
-import {getSomeUserMessages} from "../../../redux/dialogs-reducer"
+import {getSomeUserMessages, getSenderProfile, getRecipientProfile} from "../../../redux/dialogs-reducer"
 
 const DialogPrivatePage = (props) => {
+
     const dispatch = useDispatch();
-    const someUserMessages = useSelector(state => state.dialogsPage.someUserMessages);
-    // мы получили сообщения какого-то юзера, потом мы:...    
+    const dialogsPage = useSelector(state => state.dialogsPage);
+    const someUserMessages = dialogsPage.someUserMessages;
+
     const senderName = someUserMessages[0].senderName;
     const senderId = someUserMessages[0].senderId;
     const recipientId = someUserMessages[0].recipientId;
-    // Вытащили константы, которые не меняются в зависимости от сообщения.
-    const senderPhoto = null; // получив id отправителя, надо получить его аватарку
-    const recipientPhoto = null; // то же самое
+  
+    const senderPhoto = dialogsPage.senderProfile.data.photos.small;
+    const recipientPhoto = dialogsPage.recipientProfile.data.photos.small;
 
 
+    useEffect(()=>{
+        if(senderId !== undefined){
+            dispatch(getSenderProfile(senderId))
+        }
+    },[dispatch, senderId]);
 
-    // сделать useEffect, в котором if senderId != undefined, then => делай запрос пользователя по этому айди
-    // сделать useEffect, в котором if recipientId != undefined, then => делай запрос пользователя по этому айди
-    // и нам надо достать оттуда его фотки и нормальное имя.
-    // Сделать два отдельных свойства в стэйте dialogsPage.
-    
-    
+    useEffect(()=>{
+        if(recipientId !== undefined){
+            dispatch(getRecipientProfile(recipientId))
+        }
+    },[dispatch, recipientId]);
 
     useEffect(()=>{
         dispatch(getSomeUserMessages(props.match.params.dialogId))
-    },[dispatch, props.match.params.dialogId])
+    },[dispatch, props.match.params.dialogId]);
 
-    console.log("private rendered")
+    console.log("private rendered");
+    
     return (
         // header with button and aite no jouhou
         <div className={s.wrapper}>
@@ -41,10 +48,10 @@ const DialogPrivatePage = (props) => {
             </div>
 {/* messages */}
             <div className={s.mainBody}>
-                {someUserMessages.map((message)=>{ 
+                {someUserMessages.map((message, index)=>{ 
                     return (
-                        <div className={s.friendMessageWrapper}>
-                            <img className={s.friendAvatar} alt=""></img>
+                        <div className={s.friendMessageWrapper} key={index}>
+                            <img className={s.friendAvatar} src={senderPhoto} alt=""></img>
                             <div className={s.friendInnerWrapper}>
                                 <div className={s.friendMessageName}>{message.senderName}</div>
                                 <div className={s.friendMessageBody}>{message.body}</div>
@@ -61,6 +68,21 @@ const DialogPrivatePage = (props) => {
                 </div>
             </div>
             
-        </div>)
-}
+        </div>);
+};
 export default withRouter(DialogPrivatePage);
+
+
+
+// const dispatch = useDispatch();
+//     const someUserMessages = useSelector(state => state.dialogsPage.someUserMessages);
+//     // мы получили сообщения какого-то юзера, потом мы:...    
+//     const senderName = someUserMessages[0].senderName;
+//     const senderId = someUserMessages[0].senderId;
+//     const recipientId = someUserMessages[0].recipientId;
+
+//     // Вытащили константы, которые не меняются в зависимости от сообщения.
+
+//     const senderPhoto = useSelector(state => state.dialogsPage.senderProfile.data.photos.small);
+//     const recipientPhoto = useSelector(state => state.dialogsPage.recipientProfile.data.photos.small);
+
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,14 +1,34 @@
 import {dialogsAPI} from '../api/api'
+import {profileAPI} from '../api/api'
 
 const SET_DIALOGS = 'SET_DIALOGS';
 const SET_LAST_MESSAGES = 'SET_LAST_MESSAGES';
 const SET_SOME_USER_MESSAGES = 'SET_SOME_USER_MESSAGES';
+const SET_SENDER_PROFILE = 'SET_SENDER_PROFILE';
+const SET_RECIPIENT_PROFILE = 'SET_RECIPIENT_PROFILE';
+
 
 let initialState = {
     lastMessages: [],
     dialogs: [],
     initialized: false,
-    someUserMessages: [{}]
+    someUserMessages: [{}],
+    senderProfile: {
+        data: {
+            photos: {
+                small: null,
+                large: null,
+            }
+        }
+    },
+    recipientProfile: {
+    data: {
+        photos: {
+            small: null,
+            large: null,
+        }
+    }
+}
 }
 
 const dialogsReducer = (state = initialState, action) => {
@@ -32,6 +52,17 @@ const dialogsReducer = (state = initialState, action) => {
                 ...state,
                 someUserMessages: action.payload, 
             };
+        case SET_SENDER_PROFILE :
+            return {
+                ...state,
+                senderProfile: action.payload, 
+            };
+
+        case SET_RECIPIENT_PROFILE :
+            return {
+                ...state,
+                recipientProfile: action.payload,
+            };
 
         default:
             return state;
@@ -41,17 +72,34 @@ const dialogsReducer = (state = initialState, action) => {
 export const setDialogsAC = (payload) => {
   return  {type: SET_DIALOGS, payload}
 };
-
 export const setLastMessageAC = (payload) => {
     return {type: SET_LAST_MESSAGES, payload}
 };
-
 export const setSomeUserMessagesAC = payload => ({type: SET_SOME_USER_MESSAGES, payload});
+export const setSenderProfileAC = payload => ({type: SET_SENDER_PROFILE , payload});
+export const setRecipientProfileAC = payload => ({type: SET_RECIPIENT_PROFILE , payload});
+
+
+
+
+export const getSenderProfile = (senderId) => async (dispatch) => {
+  let data = await profileAPI.getProfileAPI(senderId);
+  dispatch(setSenderProfileAC(data));
+};
+
+
+export const getRecipientProfile = (recipientId) => async (dispatch) => {
+    let data = await profileAPI.getProfileAPI(recipientId);
+   dispatch(setRecipientProfileAC (data));
+};
+
 
 export const getAllDialogs = () => async (dispatch) => {
         let data = await dialogsAPI.getDialogs();
         dispatch(setDialogsAC(data));
         };
+
+
 
 export const getMessages = (dialogsArray) => async (dispatch) => {
     let data = [];
@@ -64,6 +112,8 @@ export const getMessages = (dialogsArray) => async (dispatch) => {
         dispatch(setLastMessageAC(data));
     }
 }
+
+
 export const getSomeUserMessages = (userId) => async (dispatch) => {
         let data = await dialogsAPI.getAllUserMessages(userId);
         dispatch(setSomeUserMessagesAC(data));
